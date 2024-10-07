b8b1975f9ef2dbb6ffa077a9ba209b6b1b2a667a
Thomas-Gilligan-Student <thgi236@student.oaisd.org>
2024-10-07 18:32:15 +0000

    Created program to print simple program and greeting statement

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Thomas Gilligan.");
+
+        int age = 16;
+        string greeting = "Hello World";
+        string firstname = "Thomas";
+        string lastname = "Gilligan";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + " years old. I just spent $" + money + " on an Energy Drink.");
+    }
+}
